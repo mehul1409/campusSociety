13ff27f28714f16565ba3751091cc96219f6d5f0
mehul1409 <mehulbansalswm1234@gmail.com>
2024-10-23 16:57:37 +0530

    add secure true

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -48,51 +48,53 @@ const assignSpoc = async (req, res) => {
 const adminLogin = async (req, res) => {
   const { email, password } = req.body;
   try {
-      const admin = await Admin.findOne({ email });
+    const admin = await Admin.findOne({ email });
 
-      if (!admin) {
-          return res.status(400).json({ message: 'Invalid email or password.' });
-      }
+    if (!admin) {
+      return res.status(400).json({ message: 'Invalid email or password.' });
+    }
 
-      const validPassword = await bcrypt.compare(password, admin.password);
+    const validPassword = await bcrypt.compare(password, admin.password);
 
-      if (!validPassword) {
-          return res.status(400).json({ message: 'Invalid email or password.' });
-      }
+    if (!validPassword) {
+      return res.status(400).json({ message: 'Invalid email or password.' });
+    }
 
-      const token = jwt.sign(
-          { id: admin._id },
-          process.env.JWT_SECRET,
-          { expiresIn: '1h' }
-      );
+    const token = jwt.sign(
+      { id: admin._id },
+      process.env.JWT_SECRET,
+      { expiresIn: '1h' }
+    );
 
-      res.cookie('adminToken', token,{ 
-          httpOnly: true,
-          maxAge: 3600000,
-         sameSite: 'strict' });
+    res.cookie('adminToken', token, {
+      httpOnly: true,
+      maxAge: 3600000,
+      sameSite: 'strict',
+      secure: true,
+    });
 
-      res.json({ message: 'Login successful', token });
+    res.json({ message: 'Login successful', token });
   } catch (error) {
-      res.status(500).json({ message: 'Internal server error' });
+    res.status(500).json({ message: 'Internal server error' });
   }
 };
 
 const adminRegister = async (req, res) => {
   try {
-      const { name, email, password } = req.body;
+    const { name, email, password } = req.body;
 
-      const existingAdmin = await Admin.findOne({ email });
-      if (existingAdmin) {
-          return res.status(400).json({ message: 'Admin with this email already exists.' });
-      }
+    const existingAdmin = await Admin.findOne({ email });
+    if (existingAdmin) {
+      return res.status(400).json({ message: 'Admin with this email already exists.' });
+    }
 
-      const hashedPassword = await bcrypt.hash(password, 10);
-      const newAdmin = new Admin({ name, email, password : hashedPassword});
-      await newAdmin.save();
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newAdmin = new Admin({ name, email, password: hashedPassword });
+    await newAdmin.save();
 
-      res.status(201).json({ message: 'Admin added successfully!' });
+    res.status(201).json({ message: 'Admin added successfully!' });
   } catch (error) {
-      res.status(500).json({ message: 'Error adding admin', error });
+    res.status(500).json({ message: 'Error adding admin', error });
   }
 }
 
