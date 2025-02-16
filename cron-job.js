cfa796c2e1671c4b7e9ff31cd6cc679db883e4f7
mehul1409 <mehulbansalswm1234@gmail.com>
2025-02-17 02:17:06 +0530

    update the cron job

--- cron-job.js
+++ cron-job.js
@@ -1,14 +1,17 @@
-const fetch =  require('node-fetch');
-
-const url = 'https://campussociety.onrender.com/health-check';
-
-const pingServer = async () => {
-  try {
-    const response = await fetch(url);
-    console.log(`Health check: ${response.status}`);
-  } catch (error) {
-    console.error('Error pinging server:', error);
-  }
-};
-
-pingServer();
+(async () => {
+    const fetch = (await import('node-fetch')).default;
+  
+    const url = 'https://campussociety.onrender.com/health-check';
+  
+    const pingServer = async () => {
+      try {
+        const response = await fetch(url);
+        console.log(`Health check: ${response.status}`);
+      } catch (error) {
+        console.error('Error pinging server:', error);
+      }
+    };
+  
+    pingServer();
+  })();
+  
