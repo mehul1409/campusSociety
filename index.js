c46ee67169c168ab8270d9588286e71924b55f03
teesha17 <teeshu.2179@gmail.com>
2025-02-21 22:24:07 +0530

    added send email

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const authenticateAdmin = require('./middleware/adminToken.js');
 const { getAllColleges } = require('./controllers/addCollege.js');
 const {getAllHubs,updateHub,deleteHub} = require('./controllers/hub.js')
 const {getEventsByCoordinator,getEventById} = require('./controllers/coordinatorController.js')
+const nodemailer = require('nodemailer');
 
 const app = express();
 const router = express.Router();
@@ -64,6 +65,32 @@ app.use('/admin', authenticateAdmin, adminrouter);
 app.use('/spoc', spocRouter);
 app.use('/coordinator', coordinatorRouter);
 app.use('/student', studentRouter);
+
+app.post("/send-mail", async (req, res) => {
+    const { name, email,message } = req.body;
+  
+    let transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
+  
+    let mailOptions = {
+      from: `${email}`,
+      to: "teeshakakkar2004@gmail.com",
+      subject: "Synergy 25'",
+      text: `Name: ${name}\nEmail: ${email}\n${message}`,
+    };
+  
+    try {
+      await transporter.sendMail(mailOptions);
+      res.json({ message: "Email sent successfully!" });
+    } catch (error) {
+      res.status(500).json({ message: "Failed to send email", error });
+    }
+  });
 
 app.listen(process.env.PORT, (error) => {
     if (error) {
